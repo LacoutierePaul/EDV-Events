368d8fbdd32480b0abc943831eb7e364c18c3452
mathiasbalian <mathias.balian92@gmail.com>
2023-12-07 15:18:47 +0100

    added database connection and table creation

--- backend/app.ts
+++ backend/app.ts
@@ -1,14 +1,18 @@
 import * as express from 'express';
 import { Request, Response} from 'express';
+import {connectDb, createTables} from './DbManager';
 
 
 const app = express()
 app.use(express.json())
+app.listen(3000, () => {
+    console.log(`Running on port 3000`);
+});
+
+connectDb();
+createTables().then(r => {});
 
 app.get("/api/test", (req: Request, res: Response) => {
     res.send("Salut !");
 });
 
-app.listen(3000, () => {
-    console.log(`Running on port 3000`);
-});
