ddf0196b37ec0e6c2ef2d9bbb7cfda347a267177
mathiasbalian <mathias.balian92@gmail.com>
2023-12-07 15:19:18 +0100

    created models for database

---
+++ backend/Models.ts
@@ -0,0 +1,31 @@
+import {Model} from "sequelize";
+
+export class Member extends Model {
+    declare memberId: number;
+    declare memberLastName: string;
+    declare memberFirstName: string;
+    declare memberEmail: string;
+    declare memberPassword: string;
+    declare memberSchool: string;
+    declare isAdmin: boolean;
+}
+
+export class Event extends Model {
+    declare eventId: number;
+    declare eventTitle: string;
+    declare eventPlace: string;
+    declare eventDescription?: string;
+    declare eventDate: Date;
+    declare eventLimit: number;
+}
+
+export class Participation extends Model {
+    declare eventId: number;
+    declare memberId: number;
+}
+
+export enum Schools {
+    ESILV,
+    IIM,
+    EMLV
+}
