acdc5b3f982f8dbfa0fa1e121765c7f4d86b784b
mathiasbalian <mathias.balian92@gmail.com>
2023-12-08 10:01:34 +0100

    added api endpoints for participations

--- backend/app.ts
+++ backend/app.ts
@@ -12,9 +12,92 @@ app.listen(3000, () => {
 connectDb();
 createTables().then(r => {});
 
-app.get("/api/test", (req: Request, res: Response) => {
-    res.send("Salut !");
+app.get("/api/participations", async (req: Request, res: Response) => {
+    try {
+        let participations: Participation[] = await Participation.findAll();
+        res.status(200).send(participations);
+    } catch(error){
+        res.status(500).send("Could not query the database");
+    }
 });
 
-// Participation endpoints
+app.get("/api/participations/:eventId", async (req: Request, res: Response) => {
+    try{
+        let id = +req.params.eventId;
+        let participations: Participation[] = await Participation.findAll({
+            where: {eventId: id}
+        });
 
+        if(participations){
+            res.status(200).send(participations);
+        } else {
+            res.status(404).send("Participations not found");
+        }
+    } catch(error) {
+        res.status(500).send("Wrong eventId parameter format");
+    }
+});
+
+app.get("/api/participations/:memberId", async (req: Request, res: Response) => {
+    try{
+        let id = +req.params.memberId;
+        let participations: Participation[] = await Participation.findAll({
+            where: {memberId: id}
+        });
+
+        if(participations){
+            res.status(200).send(participations);
+        } else {
+            res.status(404).send("Participations not found");
+        }
+    } catch(error) {
+        res.status(500).send("Wrong memberId parameter format");
+    }
+});
+
+app.post("/api/participations", async (req: Request, res: Response) => {
+    try {
+        const newParticipation: Participation= await Participation.create(req.body);
+        res.status(200).send(newParticipation);
+    } catch(error) {
+        res.status(500).send(error);
+    }
+});
+
+
+app.delete("/api/participations/:eventId", async (req: Request, res: Response) => {
+    try {
+        const id = +req.params.eventId;
+        await Participation.destroy({
+            where: {eventId: id}
+        });
+    } catch (error) {
+        res.status(500).send("Wrong id parameter format");
+    }
+});
+
+app.delete("/api/participations/:memberId", async (req: Request, res: Response) => {
+    try {
+        const id = +req.params.memberId;
+        await Participation.destroy({
+            where: {memberId: id}
+        });
+    } catch (error) {
+        res.status(500).send("Wrong id parameter format");
+    }
+});
+
+app.delete("/api/participations/:memberId/:eventId", async (req: Request, res: Response) => {
+    try {
+        const memberId = +req.params.memberId;
+        const eventId = +req.params.eventId;
+        await Participation.destroy({
+            where: {
+                memberId: memberId,
+                eventId: eventId
+            }
+        });
+    } catch (error) {
+        res.status(500).send("Wrong id parameter format");
+    }
+});
