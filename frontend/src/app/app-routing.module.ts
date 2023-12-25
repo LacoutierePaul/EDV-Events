58f54e49cdf1f3100c0030151ad0de026d051af0
mathiasbalian <mathias.balian92@gmail.com>
2023-12-25 23:37:40 +0100

    added module declarations and routing for connection page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {PresentationComponent} from "./presentation/presentation.component";
+import {ConnectionPageComponent} from "./connection-page/connection-page.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'presentation', component: PresentationComponent}
+  {path: 'presentation', component: PresentationComponent},
+  {path: 'connection', component: ConnectionPageComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { HomeComponent } from './home/home.component';
 import { PresentationComponent } from './presentation/presentation.component';
 import { TournamentsComponent } from './tournaments/tournaments.component';
 import { TrainingsComponent } from './trainings/trainings.component';
+import { ConnectionPageComponent } from './connection-page/connection-page.component';
+import { ConnectionFormComponent } from './connection-form/connection-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -18,12 +22,17 @@ import { TrainingsComponent } from './trainings/trainings.component';
     HomeComponent,
     PresentationComponent,
     TournamentsComponent,
-    TrainingsComponent
+    TrainingsComponent,
+    ConnectionPageComponent,
+    ConnectionFormComponent,
+    RegisterFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
