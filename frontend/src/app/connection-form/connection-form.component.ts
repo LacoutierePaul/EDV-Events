cd73298d792f105936f9042455e7803ba123ebee
mathiasbalian <mathias.balian92@gmail.com>
2023-12-25 23:37:58 +0100

    created connection form component

---
+++ frontend/src/app/connection-form/connection-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-form',
+  templateUrl: './connection-form.component.html',
+  styleUrls: ['./connection-form.component.css']
+})
+export class ConnectionFormComponent {
+
+}
