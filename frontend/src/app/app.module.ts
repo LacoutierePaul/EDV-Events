246e35e350be5ab536bb8daa4a7c4fbe97b4abdd
mathiasbalian <mathias.balian92@gmail.com>
2023-12-25 19:24:28 +0100

    added components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { EventsComponent } from './events/events.component';
+import { HomeComponent } from './home/home.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { TournamentsComponent } from './tournaments/tournaments.component';
+import { TrainingsComponent } from './trainings/trainings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    EventsComponent,
+    HomeComponent,
+    PresentationComponent,
+    TournamentsComponent,
+    TrainingsComponent
   ],
   imports: [
     BrowserModule,
