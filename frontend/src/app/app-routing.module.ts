9fac0ad85de48a06c8757a61e0bc36eeed4f5fb3
mathiasbalian <mathias.balian92@gmail.com>
2023-12-25 18:14:38 +0100

    added frontend to project

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {TournamentsComponent} from "./tournaments/tournaments.component";
-import {TrainingsComponent} from "./trainings/trainings.component";
-import {PresentationComponent} from "./presentation/presentation.component";
-import {EventsComponent} from "./events/events.component";
 
-const routes: Routes = [{path: 'presentation', component: PresentationComponent},{path:'home',component:HomeComponent},
-  {path:'tournaments',component:TournamentsComponent},{path:'trainings',component:TrainingsComponent},
-  {path:'events',component:EventsComponent}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventsComponent } from './events/events.component';
-import { PresentationComponent } from './presentation/presentation.component';
-import { HomeComponent } from './home/home.component';
-import { TournamentsComponent } from './tournaments/tournaments.component';
-import { TrainingsComponent } from './trainings/trainings.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventsComponent,
-    PresentationComponent,
-    HomeComponent,
-    TournamentsComponent,
-    TrainingsComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
