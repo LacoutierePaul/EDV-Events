1ce2f201e451404a8e79b7e837d802d7f0fd54c5
mathiasbalian <mathias.balian92@gmail.com>
2023-12-25 23:38:24 +0100

    created register form component

---
+++ frontend/src/app/register-form/register-form.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
+})
+export class RegisterFormComponent {
+}
