38d9f18a2c316922729e2b1d861c3006196770cd
mathiasbalian <mathias.balian92@gmail.com>
2023-12-26 18:30:33 +0100

    added routing to trainings page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {HomeComponent} from "./pages/home/home.component";
 import {PresentationComponent} from "./pages/presentation/presentation.component";
 import {ConnectionPageComponent} from "./pages/connection-page/connection-page.component";
 import {TournamentsComponent} from "./pages/tournaments/tournaments.component";
+import {TrainingsComponent} from "./pages/trainings/trainings.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'presentation', component: PresentationComponent},
   {path: 'connection', component: ConnectionPageComponent},
-  {path: 'tournaments', component: TournamentsComponent}
+  {path: 'tournaments', component: TournamentsComponent},
+  {path: 'trainings', component: TrainingsComponent}
 ];
 
 @NgModule({
