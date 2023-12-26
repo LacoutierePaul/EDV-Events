6c5a3bd2f1c75c3f551061687e2cdfda74dd44e0
mathiasbalian <mathias.balian92@gmail.com>
2023-12-26 16:56:20 +0100

    added routing to tournaments page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {PresentationComponent} from "./presentation/presentation.component";
-import {ConnectionPageComponent} from "./connection-page/connection-page.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {PresentationComponent} from "./pages/presentation/presentation.component";
+import {ConnectionPageComponent} from "./pages/connection-page/connection-page.component";
+import {TournamentsComponent} from "./pages/tournaments/tournaments.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'presentation', component: PresentationComponent},
-  {path: 'connection', component: ConnectionPageComponent}
+  {path: 'connection', component: ConnectionPageComponent},
+  {path: 'tournaments', component: TournamentsComponent}
 ];
 
 @NgModule({
