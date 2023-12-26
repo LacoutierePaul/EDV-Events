2c7d96e1da6c7718f073476da7ac5c30d4b3d7dc
mathiasbalian <mathias.balian92@gmail.com>
2023-12-26 18:38:57 +0100

    improved trainings page

--- frontend/src/app/pages/trainings/trainings.component.ts
+++ frontend/src/app/pages/trainings/trainings.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Event} from "../../app.component";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-trainings',
   templateUrl: './trainings.component.html',
   styleUrls: ['./trainings.component.css']
 })
-export class TrainingsComponent {
+export class TrainingsComponent implements OnInit{
 
+  trainingsList: Event[] = [];
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit(): void {
+    this.getTournaments();
+  }
+
+  getTournaments(){
+    this.httpClient.get<Event[]>("/api/eventsByType/Training").subscribe({
+      next: (res: Event[]) => {
+        this.trainingsList = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
