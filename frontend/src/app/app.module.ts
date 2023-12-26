0f1026bba4094281f8f8aa671c8d5f2df33cc953
mathiasbalian <mathias.balian92@gmail.com>
2023-12-26 16:55:26 +0100

    added components declaration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { EventsComponent } from './events/events.component';
-import { HomeComponent } from './home/home.component';
-import { PresentationComponent } from './presentation/presentation.component';
-import { TournamentsComponent } from './tournaments/tournaments.component';
-import { TrainingsComponent } from './trainings/trainings.component';
-import { ConnectionPageComponent } from './connection-page/connection-page.component';
-import { ConnectionFormComponent } from './connection-form/connection-form.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EventsComponent } from './pages/events/events.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PresentationComponent } from './pages/presentation/presentation.component';
+import { TournamentsComponent } from './pages/tournaments/tournaments.component';
+import { TrainingsComponent } from './pages/trainings/trainings.component';
+import { ConnectionPageComponent } from './pages/connection-page/connection-page.component';
+import { ConnectionFormComponent } from './components/connection-form/connection-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventHolderComponent } from './components/event-holder/event-holder.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -26,15 +29,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     TrainingsComponent,
     ConnectionPageComponent,
     ConnectionFormComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    EventHolderComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
