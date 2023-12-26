0e1f60d4d461cae67f81970d4ca32082e362c329
mathiasbalian <mathias.balian92@gmail.com>
2023-12-26 18:34:38 +0100

    created other events page

---
+++ frontend/src/app/pages/other-events/other-events.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-events',
+  templateUrl: './other-events.component.html',
+  styleUrls: ['./other-events.component.css']
+})
+export class OtherEventsComponent {
+
+}
