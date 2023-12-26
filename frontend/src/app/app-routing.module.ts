f24b6fc46065b45999cf1767cb4c5674e4efa680
mathiasbalian <mathias.balian92@gmail.com>
2023-12-26 19:35:26 +0100

    added routing to other events page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {PresentationComponent} from "./pages/presentation/presentation.component
 import {ConnectionPageComponent} from "./pages/connection-page/connection-page.component";
 import {TournamentsComponent} from "./pages/tournaments/tournaments.component";
 import {TrainingsComponent} from "./pages/trainings/trainings.component";
+import {OtherEventsComponent} from "./pages/other-events/other-events.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'presentation', component: PresentationComponent},
   {path: 'connection', component: ConnectionPageComponent},
   {path: 'tournaments', component: TournamentsComponent},
-  {path: 'trainings', component: TrainingsComponent}
+  {path: 'trainings', component: TrainingsComponent},
+  {path: 'other-events', component: OtherEventsComponent}
 ];
 
 @NgModule({
