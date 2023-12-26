95bb9f83dfec8479afb3a87055cc602021b96738
mathiasbalian <mathias.balian92@gmail.com>
2023-12-26 19:35:52 +0100

    improved other events page

--- frontend/src/app/pages/other-events/other-events.component.ts
+++ frontend/src/app/pages/other-events/other-events.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Event} from "../../app.component";
 
 @Component({
   selector: 'app-other-events',
   templateUrl: './other-events.component.html',
   styleUrls: ['./other-events.component.css']
 })
-export class OtherEventsComponent {
+export class OtherEventsComponent implements OnInit{
+
+  otherEventsList: Event[] = [];
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit(): void {
+    this.getOtherEvents();
+  }
+
+  getOtherEvents(){
+    this.httpClient.get<Event[]>("/api/eventsByType/Event").subscribe({
+      next: (res: Event[]) => {
+        this.otherEventsList = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 
 }
